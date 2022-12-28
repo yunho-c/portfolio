fd24f999f4165ce1c3d5274e31cec40a64017a22
k2m5t2 <yhjc2692@gmail.com>
2022-12-28 01:56:19 -0500

    Content Update

--- src/pages/about_.md
+++ src/pages/about_.md
@@ -8,7 +8,7 @@ title: About
 </div>
 
 <!-- Thanks for visiting my website. My name is Yunho Cho. I’m a college student studying mechanical engineering and psychology at Georgia Institute of Technology. I hope you find my work and the content of this website interesting. If you want to know a little more about me, read on below or get in touch! -->
-Thanks for visiting my website. My name is Yunho Cho, and I study mechanical engineering and psychology at Georgia Institute of Technology. I hope you find my work and the content of this website interesting. If you want to know a little more about me, read on below or get in touch!
+Thanks for visiting my website. My name is Yunho Cho, and I study mechanical engineering and psychology at Georgia Institute of Technology. I hope you find my work and my story interesting. If you want to know a little more about me, read on below or get in touch!
 
 <!-- [Vitesse](https://github.com/antfu/vitesse) is an opinionated [Vite](https://github.com/vitejs/vite) starter template made by [@antfu](https://github.com/antfu) for mocking apps swiftly. With **file-based routing**, **components auto importing**, **markdown support**, I18n, PWA and uses **UnoCSS** for styling and icons. -->
 
@@ -21,3 +21,21 @@ function vitesse() {
 ```
 
 Check out the [GitHub repo](https://github.com/antfu/vitesse) for more details. -->
+
+<div class="text-center">
+  <h3>Personal Background</h3>
+</div>
+
+asdf
+
+<div class="text-center">
+  <h3>Vision</h3>
+</div>
+
+jkl;
+
+<div class="text-center">
+  <h3>Pseudo-Statement of Purpose</h3>
+</div>
+
+1234
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -23,7 +23,9 @@ const { t } = useI18n()
           Mechanical Engineering '24 @ <span class="text-yellow">Georgia Tech</span>
         </h3>
         <p class="mt-2 text-sm text-gray-300">
-          [Some description text. Some dummy text here.]
+          <!-- [Some description text. Some dummy text here.] -->
+          Minor in Psychology | Planned: Master's in Human Computer Interaction (HCI)
+          <!-- Passionate about design, technology, and education. -->
         </p>
       </div>
     </div>
@@ -41,7 +43,7 @@ const { t } = useI18n()
     <div py-4 />
 
     <div class="grid grid-cols-3 gap-6">
-      <div>
+      <!-- <div>
         <h2 class="text-2xl font-semibold op80">
           About
         </h2>
@@ -55,9 +57,39 @@ const { t } = useI18n()
         <h2 class="text-2xl font-semibold op80">
           Projects
         </h2>
-      </div>
+      </div> -->
 
-      <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+      <div class="max-w-sm p-6 bg-white rounded-lg shadow-md hover:shadow-xl transition-all duration-200 dark:bg-gray-800 dark:border-gray-700">
+        <a href="#">
+          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white op80">About</h5>
+        </a>
+        <!-- <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">It is possible to never work a day in life if you truly enjoy what you do. I find value and meaning in helping people solve problems using whatever means I have—which is oftentimes technology. Please allow me to introduce myself!</p> -->
+        <!-- <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">It is possible to never work a day in life if you truly enjoy what you do. I find value and meaning in helping people solve problems using whatever means I have—which is oftentimes technology.</p> -->
+        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">It is possible to never work a day in life if you truly enjoy what you do. I find value and meaning in helping people solve problems using whatever means at disposal—whether it's technology, communication, or a new way to combine both.</p>
+        <!-- <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Read more</a> -->
+        <a href="#" class="inline-flex items-center mt-2 px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Allow me to introduce myself!</a>
+      </div>
+      <div class="max-w-sm p-6 bg-white rounded-lg shadow-md hover:shadow-xl transition-all duration-200 dark:bg-gray-800 dark:border-gray-700">
+        <a href="#">
+          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white op80">Coursework</h5>
+        </a>
+        <!-- <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">When I entered college, I promised myself to take classes that were the most unique, interesting, and of value—even if that meant stepping out of my comfort zone or sacrificing my GPA. I am happy to report that I have never regretted my decision.</p> -->
+        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">When I entered college, I promised myself to take classes that were the most unique, interesting, and of value—even if that meant stepping out of my comfort zone or sacrificing my GPA. Looking back, that decision has made all the difference.</p>
+        <!-- <a href="#" class="inline-flex items-center mt-2 px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Take a look!</a> -->
+        <a href="#" class="inline-flex items-center mt-2 px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Curious in what I've learned?</a>
+      </div>
+      <div class="max-w-sm p-6 bg-white rounded-lg shadow-md hover:shadow-xl transition-all duration-200 dark:bg-gray-800 dark:border-gray-700">
+        <a href="#">
+          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white op80">Projects</h5>
+        </a>
+        <!-- <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">asdf</p> -->
+        <!-- <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">'Project Based Learning' is one of my favorite words. Why? It's where the different topics of studies and the various people in the world intersect</p> -->
+        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">'Project Based Learning' is one of my favorite words. Why? Because the process of ideating, building, and testing a product represents an intersection whereby all the different topics of studies and the various peoples of our world connect. </p>
+        <a href="#" class="inline-flex items-center mt-2 px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Take a look at my projects!</a>
+      </div>
+    </div>
+
+      <!-- <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
         <a href="#">
           <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
         </a>
@@ -69,11 +101,7 @@ const { t } = useI18n()
           <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd" /></svg>
         </a>
       </div>
-    </div>
-
-    <div py-4 />
-
-    [about]
+    </div> -->
 
     <div py-4 />
 
